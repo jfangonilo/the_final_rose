72b02c0e5dd72fe2c309be6ba584b3c157e4b273
Jomah Fangonilo <jomah.fangonilo@gmail.com>
2020-01-03 11:39:07 -0700

    User Story 1

---
+++ app/controllers/contestants_controller.rb
@@ -0,0 +1,8 @@
+class ContestantsController < ApplicationController
+
+  def index
+    bachelorette = Bachelorette.find(params[:bachelorette_id])
+    @contestants = bachelorette.contestants
+  end
+
+end
